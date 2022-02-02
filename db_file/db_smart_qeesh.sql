003d77072772bf51d755fbd3ac18cdb2a79a58ea
yrzoktan <rz.oktan@gmail.com>
2022-02-02 21:51:28 +0700

    update db for menu management

--- db_file/db_smart_qeesh.sql
+++ db_file/db_smart_qeesh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 31 Jan 2022 pada 15.13
+-- Waktu pembuatan: 02 Feb 2022 pada 15.50
 -- Versi server: 10.4.21-MariaDB
 -- Versi PHP: 8.0.10
 
@@ -20,6 +20,46 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_smart_qeesh`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menu`
+--
+
+CREATE TABLE `menu` (
+  `id_menu` int(11) NOT NULL,
+  `nama_menu` varchar(20) NOT NULL,
+  `link_menu` text NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `color` varchar(50) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `editable` enum('N/A','YES','','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `menu`
+--
+
+INSERT INTO `menu` (`id_menu`, `nama_menu`, `link_menu`, `type`, `color`, `icon`, `is_active`, `editable`) VALUES
+(1, 'Sync Data Simak', 'sync-simak', 'statis', '', 'fa fa-sync', 0, 'YES'),
+(2, 'Dashboard', 'dashboard', 'statis', '2B7B70', 'gi gi-dashboard', 1, 'YES');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `submenu`
+--
+
+CREATE TABLE `submenu` (
+  `id_submenu` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `nama_submenu` varchar(26) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL,
+  `is_active` int(1) NOT NULL COMMENT 'untuk status menu'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -47,6 +87,26 @@ INSERT INTO `users` (`user_id`, `username`, `password`, `role_id`, `user_detail_
 (1, 'admin', '202cb962ac59075b964b07152d234b70', 1, 1, 1643410800, 1, 0, 0),
 (2, 'admin1', '21232f297a57a5a743894a0e4a801fc3', 1, 2, 1643410800, 0, 0, 0),
 (3, 'admintest', '202cb962ac59075b964b07152d234b70', 1, 3, 1643583600, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `id_menu`) VALUES
+(1, 1, 1),
+(2, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -98,11 +158,32 @@ INSERT INTO `user_role` (`role_id`, `role_type`, `description`) VALUES
 --
 
 --
+-- Indeks untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id_menu`),
+  ADD KEY `id_menu` (`id_menu`,`is_active`);
+
+--
+-- Indeks untuk tabel `submenu`
+--
+ALTER TABLE `submenu`
+  ADD PRIMARY KEY (`id_submenu`),
+  ADD KEY `id_submenu` (`id_submenu`,`id_menu`,`is_active`);
+
+--
 -- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`user_id`),
   ADD KEY `user_id` (`user_id`,`role_id`,`user_detail_id`,`is_active`);
+
+--
+-- Indeks untuk tabel `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id` (`id`,`role_id`,`id_menu`);
 
 --
 -- Indeks untuk tabel `user_detail`
@@ -114,17 +195,36 @@ ALTER TABLE `user_detail`
 -- Indeks untuk tabel `user_role`
 --
 ALTER TABLE `user_role`
-  ADD PRIMARY KEY (`role_id`);
+  ADD PRIMARY KEY (`role_id`),
+  ADD KEY `role_id` (`role_id`,`role_type`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
+-- AUTO_INCREMENT untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id_menu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `submenu`
+--
+ALTER TABLE `submenu`
+  MODIFY `id_submenu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `user_detail`
