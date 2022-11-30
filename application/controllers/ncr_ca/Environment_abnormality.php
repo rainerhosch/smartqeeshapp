8a01b6ee68ba8aae2a2c918a5bf32b11e48c1253
typing-art <typingart.94@gmail.com>
2022-11-30 09:56:48 +0700

    new env abnormality modul

--- application/controllers/ncr_ca/Environment_abnormality.php
+++ application/controllers/ncr_ca/Environment_abnormality.php
@@ -9,12 +9,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
  *  Date Created          : 07/05/2022
  *  Quots of the code     : 'rapihkan lah code mu, seperti halnya kau menata kehidupan'
  */
+
+use PhpOffice\PhpWord\PhpWord;
+use PhpOffice\PhpWord\Shared\Html;
+use PhpOffice\PhpWord\Writer\Word2007;
+
+use PhpOffice\PhpWord\Element\Field;
+use PhpOffice\PhpWord\Element\Table;
+use PhpOffice\PhpWord\Element\TextRun;
+use PhpOffice\PhpWord\SimpleType\TblWidth;
+
 class Environment_Abnormality extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         login_check();
+        $this->load->model('M_Envinvestigation', 'envinvestigation');
     }
 
     public function index()
@@ -25,6 +36,17 @@ class Environment_Abnormality extends CI_Controller
         $data['page'] = 'NCR & CA';
         $data['subpage'] = 'Environment Abnormality';
         $data['content'] = 'pages/ncr_ca/v_environment_abnormal';
+        $jml = $this->envinvestigation->get_data()->num_rows();
+        $data['erca_no'] = 'E-RCA-' . ($jml + 1);
         $this->load->view('template', $data);
+    }
+
+    public function save_data()
+    {
+        $data_post = $this->input->post();
+        echo '<pre>';
+        var_dump($data_post);
+        echo '</pre>';
+        die;
     }
 }
