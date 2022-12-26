9205e3c7c6b5dba393f116f79a05f2fd63cc8f6f
typing-art <typingart.94@gmail.com>
2022-12-26 15:18:59 +0700

    update routes & sidebar

--- application/views/layouts/sidebar.php
+++ application/views/layouts/sidebar.php
@@ -43,6 +43,49 @@
 
 
 
+        // $.ajax({
+        //     type: "POST",
+        //     url: "<?= base_url() ?>menu/get_user_access_menu",
+        //     dataType: "json",
+        //     success: function(response) {
+        //         if (response.status === true) {
+        //             if (response.data != 0) {
+        //                 let base_url = `<?= base_url(); ?>`;
+        //                 let url = $(location).attr('href').split("/").splice(0, 10).join("/");
+        //                 let segments = url.split('/');
+        //                 let numb = segments.length - 1;
+        //                 let html = ``;
+        //                 let class_active = ``;
+        //                 $.each(response.data, function(i, menu) {
+        //                     if (menu.type != 'statis') {
+        //                         html += `<li class="nav-header"><strong><a href="${base_url + menu.link_menu}">${menu.nama_menu}</a></strong></li>`;
+        //                     } else {
+        //                         html += `<li class="nav-header"><strong>${menu.nama_menu}</strong></li>`;
+        //                         if (menu.submenu != 0) {
+        //                             $.each(menu.submenu, function(i, sm) {
+        //                                 let nm_submenu = sm.nama_submenu;
+        //                                 nm_submenu = nm_submenu.replace(/ /g, "_");
+        //                                 html += `<li class="nav-item">`;
+        //                                 if (nm_submenu.toLowerCase() === segments[numb].toLowerCase()) {
+        //                                     html += `<a href="<?= base_url() ?>${sm.url}" class="nav-link active">`;
+        //                                 } else {
+        //                                     html += `<a href="<?= base_url() ?>${sm.url}" class="nav-link ">`;
+        //                                 }
+        //                                 html += `${sm.icon} `;
+        //                                 html += `<p class="ml-3">${sm.nama_submenu}</p>`;
+        //                                 html += `</a>`;
+        //                                 html += `</li>`;
+        //                                 html += `</li>`;
+        //                             });
+        //                         }
+        //                     }
+        //                 });
+        //                 $(".sidebar_menu").html(html);
+        //             }
+        //         }
+        //     }
+        // });
+        
         $.ajax({
             type: "POST",
             url: "<?= base_url() ?>menu/get_user_access_menu",
@@ -53,32 +96,64 @@
                         let base_url = `<?= base_url(); ?>`;
                         let url = $(location).attr('href').split("/").splice(0, 10).join("/");
                         let segments = url.split('/');
+                        console.log(segments);
                         let numb = segments.length - 1;
                         let html = ``;
                         let class_active = ``;
                         $.each(response.data, function(i, menu) {
+                            // console.log(menu);
                             if (menu.type != 'statis') {
-                                html += `<li class="nav-header"><strong><a href="${base_url + menu.link_menu}">${menu.nama_menu}</a></strong></li>`;
+                                let activeMenu1 = `nav-item`;
+
+                                if (segments[3] == menu.link_menu) {
+                                    activeMenu1 = `nav-item menu-is-opening menu-open`;
+                                }
+
+                                html += `<li class="${activeMenu1}">`;
+                                html += `<a href="${base_url + menu.link_menu}" class="nav-link">`;
+                                html += `<p>`;
+                                html += menu.nama_menu;
+                                html += `</p>`;
+                                html += `</a>`;
+                                html += `</li>`;
+
                             } else {
-                                html += `<li class="nav-header"><strong>${menu.nama_menu}</strong></li>`;
+                                let activeMenu2 = `nav-item`;
+
+                                if (segments[3] == menu.link_menu) {
+                                    activeMenu2 = `nav-item menu-is-opening menu-open`;
+                                }
+
+                                html += `<li class="${activeMenu2}">`;
+                                html += `<a href="${base_url + menu.link_menu}" class="nav-link">`;
+                                html += `<p> `;
+                                html += menu.nama_menu;
+                                html += `<i class="fas fa-angle-left right"></i>`;
+                                html += `</p>`;
+                                html += `</a>`;
+                                html += `<ul class="nav nav-treeview">`;
+
                                 if (menu.submenu != 0) {
                                     $.each(menu.submenu, function(i, sm) {
                                         let nm_submenu = sm.nama_submenu;
                                         nm_submenu = nm_submenu.replace(/ /g, "_");
                                         html += `<li class="nav-item">`;
+
                                         if (nm_submenu.toLowerCase() === segments[numb].toLowerCase()) {
                                             html += `<a href="<?= base_url() ?>${sm.url}" class="nav-link active">`;
                                         } else {
                                             html += `<a href="<?= base_url() ?>${sm.url}" class="nav-link ">`;
                                         }
+
                                         html += `${sm.icon} `;
-                                        html += `<p class="ml-3">${sm.nama_submenu}</p>`;
+                                        html += `<p class="ml-1">${sm.nama_submenu}</p>`;
                                         html += `</a>`;
-                                        html += `</li>`;
                                         html += `</li>`;
                                     });
                                 }
+                                html += `</ul></li>`;
                             }
+
                         });
                         $(".sidebar_menu").html(html);
                     }
